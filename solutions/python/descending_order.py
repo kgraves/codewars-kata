be03afb119a1c78eba49b63571959e7e74550038
kg <kylgrvs@gmail.com>
2017-05-24 22:21:29 -0700

    updates descending_order

--- solutions/python/descending_order.py
+++ solutions/python/descending_order.py
@@ -1,5 +1,5 @@
 def descending_order(num):
-    return int(''.join([ n for n in reversed(str(num)) ]))
+    return int(''.join(sorted(str(num), reverse=True)))
 
 import unittest
 class TestIsBalanced(unittest.TestCase):
@@ -8,6 +8,7 @@ class TestIsBalanced(unittest.TestCase):
         self.assertEquals(descending_order(0), 0)
         self.assertEquals(descending_order(15), 51)
         self.assertEquals(descending_order(123456789), 987654321)
+        self.assertEquals(descending_order(1201), 2110)
 
 if __name__ == '__main__':
     unittest.main()
